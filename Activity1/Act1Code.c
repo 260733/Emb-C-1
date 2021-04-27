41e72b8f09f349a60789fc17fb9fa88fc11cb5c0
Damanpreet Singh <damanpreet1495@gmail.com>
2021-04-27 13:09:16 +0530

    code

---
+++ Activity1/Act1Code.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include<util/delay.h>
+
+int main(void)
+{
+DDRB|=(1<<PB0);//setting B0=1 when led will glow
+DDRB&=~(1<<PB0);//clearing the bit
+PORTB|=(1<<PB6);//set the bit
+DDRB&=~(1<<PB7);//clear bit
+PORTB|=(1<<PB7);//set the bit
+
+    while(1)
+        {
+            if(!(PINB&(1<<PB7)))
+            {
+                if(!(PINB&(1<<PB6)))
+                   {
+
+                       PORTB|=(1<<PB0);//Indication that seat and heater both on
+                       _delay_ms(10);
+
+                   }
+                   else
+                    {
+            PORTB&=~(1<<PB0);
+            _delay_ms(10);
+        }
+            }
+
+        }
+
+return 0;
+}
